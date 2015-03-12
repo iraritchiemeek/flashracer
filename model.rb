cb873022682d7cd723748f0d2a9486ea20235ae5
Geordi Dearns & iraritchiemeek <pair+geordidearns.iraritchiemeek@devbootcamp.com>
2015-03-13 11:36:34 +1300

    Initializes 2 players

--- model.rb
+++ model.rb
@@ -31,9 +31,9 @@ end
 class FlashModel
 
   def initialize
- #create a players array
- # makes the players (2) and push into players array
- #call file parsing method on init
+   @players = [] # make an empty players array which both player will be pushed into
+   2.times do {@players << Player.new} # two time smake a new player and push them into the empty array
+   file_parsing # call the file parsing method
   end
 
   def file_parsing
@@ -43,7 +43,7 @@ class FlashModel
       question_chunk = questions.slice!(0,3) # question chunks is each line in the file, and splitting them at 0 and 3 index giving me 3 lines
       @flashcards << FlashCard.new(question_chunk[0],question_chunk[1]) # pushing the question[0] and answer[1] into flashcards array
     end
-    @flashcards.shuffle # shuffle the cards randomly to return a different question each time called
+    # @flashcards.shuffle! # shuffle the cards randomly to return a different question each time called
   end
 
   # def display_score
@@ -63,11 +63,13 @@ class FlashModel
   # end
 
   def get_current_card
-    #takes the
+    @flashcards.sample
   end
 
 end
 ##############################################################
 
 test = FlashModel.new
-p test.file_parsing
+test.file_parsing
+p test.get_current_card
+
