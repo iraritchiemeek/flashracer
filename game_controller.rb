8b2b0f751307e44f6d54e97936bcac958e01dd6b
Alexandre Barret & Pete Duncan <pair+alexb52.petedigipulse@devbootcamp.com>
2015-03-13 12:32:11 +1300

    complete welcoming

--- game_controller.rb
+++ game_controller.rb
@@ -1,7 +1,7 @@
 require_relative 'view'
 require_relative 'model'
 class GameController
-attr_reader :view, :name
+attr_reader :view, :model
   def initialize(game_model, game_view)
     @game = game_model
     @view = game_view
@@ -10,21 +10,21 @@ attr_reader :view, :name
   def start
 
     view.initialize_game_render
-    # get user input for the player 1
-    # get user input for the player 2
     player_names = view.enter_names_render
-    model.add_player_names(player_names)
+    # model.add_player_names(player_names)
+
     # render name of players
     view.render_player_names(player_names)
+
     # render score of players
     # ask model for the players score
-    view.render_scores#(scores)
+    # view.render_score(model.get_player(0))
     # say which player is playing
       # ask model players array
       # ask view to print the of the player! (players[0].name)
     # print question
       #ask Flashcards for a deck
-      ask view to dislay question
+           # ask view to dislay question
     # get user input for answer
     # checking if answer is right
       # if right display "RIGHT" and score and racer moves
@@ -42,3 +42,5 @@ my_game = FlashModel.new
 my_view = View.new
 
 my_controller  = GameController.new(my_game, my_view)
+
+my_controller.start
