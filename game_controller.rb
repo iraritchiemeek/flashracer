c15397efe34ed2b5ef0d57dd3be293abccaef839
Alexandre Barret & Pete Duncan <pair+alexb52.petedigipulse@devbootcamp.com>
2015-03-13 10:53:31 +1300

    Add view to controller

--- game_controller.rb
+++ game_controller.rb
@@ -1,3 +1,4 @@
+require_relative 'view'
 class GameController
 
   def initialize(game_model, game_view)
@@ -10,6 +11,6 @@ end
 # Create the model
 my_game = ""
 # Create the view
-my_view = ""
+my_view = View.new
 
 my_controller  = GameController.new(my_game, my_view)
