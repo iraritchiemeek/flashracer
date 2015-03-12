cc9ac48c93c604fecb61dbd5d09666e4043ce924
Alexandre Barret & Pete Duncan <pair+alexb52.petedigipulse@devbootcamp.com>
2015-03-13 11:11:22 +1300

    Comment Basic logic of the game

--- game_controller.rb
+++ game_controller.rb
@@ -6,6 +6,24 @@ class GameController
     @view = game_view
   end
 
+  def start
+
+    # initialize game
+    # get user input for the player 1
+    # get user input for the player 2
+    # render name of players
+    # render score of players
+    # say which player is playing
+    # print question
+    # get user input for answer
+    # checking if answer is right
+      # if right display "RIGHT" and score and racer moves
+      # if wrong display "WRONG" and display right answer
+    # Switch player
+    # Repeat from 16 to 22 until one of the racer reaches the end of the line or the no more questions left
+
+  end
+
 end
 
 # Create the model
